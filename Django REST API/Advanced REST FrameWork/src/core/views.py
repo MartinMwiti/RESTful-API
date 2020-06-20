7ed6e2a8ae505fc4c1eb424f673386670878778d
MartinMwiti <martinmwiti007@gmail.com>
2020-06-20 22:06:27 +0300

    update

--- Django REST API/Advanced REST FrameWork/src/core/views.py
+++ Django REST API/Advanced REST FrameWork/src/core/views.py
@@ -9,3 +9,4 @@ def test_view(request):
     }
     # Here i'm passing dict. To pass another type say list. Add 'safe=False' next to data val inside the JsonResponse.
     return JsonResponse(data)
+    #  You can view the JSON data by runing the server or use the route say, 'curl http://localhost:8000' if the route was set to blank 
