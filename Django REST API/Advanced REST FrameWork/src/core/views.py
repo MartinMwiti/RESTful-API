ccf7ee434dec6f34eaca3c17f791561005ebdf59
MartinMwiti <martinmwiti007@gmail.com>
2020-06-20 20:57:52 +0300

    views

--- Django REST API/Advanced REST FrameWork/src/core/views.py
+++ Django REST API/Advanced REST FrameWork/src/core/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 def test_view(request):
-    return
+    data = {
+        'name': 'Martin',
+        'age': 26
+    }
+    # Here i'm passing dict. To pass another type say list. Add 'safe=False' next to data val inside the JsonResponse.
+    return JsonResponse(data)
