ee7cc25babedea086e5439f60569e6869a16c844
MartinMwiti <martinmwiti007@gmail.com>
2020-06-20 23:21:07 +0300

    serializers update

--- Django REST API/Advanced REST FrameWork/src/core/serializers.py
+++ Django REST API/Advanced REST FrameWork/src/core/serializers.py
@@ -1,2 +1,24 @@
+#  very similar to how you construct 'form'
 from rest_framework import serializers
+from .models import Post
 
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            'title', 
+            'description',
+            'author'
+        )
+
+
+
+
+
+
+
+
+'''
+PostSerializer represents a transformation between my 'Post' model into a Json payload that contains the above stated fields. Similar to forms which represents a transformation between my 'Post' model into a form that contains the above stated fields
+'''
--- Django REST API/Advanced REST FrameWork/src/core/views.py
+++ Django REST API/Advanced REST FrameWork/src/core/views.py
@@ -5,15 +5,33 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import PostSerializer
+from .models import Post
+
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            'name': 'Martin',
-            'age': 26
-        }
+        qs = Post.objects.all()
+        # post = qs.first() # To get the 1st instance
+        # serializer = PostSerializer(post)
+        serializer = PostSerializer(qs, many=True) # Here i'm serializing a queryset of the 'POST' model. if passing many queries, add 'many=True'
+        return Response(serializer.data) # Response inherits from 'JsonResponse'
 
-        return Response(data) # Response inherits from 'JsonResponse'
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors)
+
+
+
+
+
+
+
+
 
 
 
